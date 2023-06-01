4e55a159b37f852ac45d914780f8a17e47777d7e
VruttiTanna <tannavrutti@gmail.com>
2023-06-02 00:54:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,62 +43,57 @@ def process_file(file_path):
 
 
 # Streamlit application setup
-st.set_page_config(page_title='Chatbot with PDF Support', layout='wide')
 st.title('Chatbot with PDF Support')
 
-# Sidebar - OpenAI API Key
+# OpenAI API Key
 st.sidebar.header('OpenAI API Key')
-api_key = st.sidebar.text_input('Enter your OpenAI API key')
+api_key = st.sidebar.text_input('Upload your OpenAI API key')
 if api_key:
     set_apikey(api_key)
 
-# Main Section - Chatbot and PDF Upload
-col1, col2 = st.beta_columns([2, 1])
+# Chatbot and Image Display
+st.subheader('Chatbot')
+txt = st.text_input('Enter text and press enter')
+chat_history = []
 
-# Chatbot Section
-with col1:
-    st.subheader('Chatbot')
-    txt = st.text_input('Enter text and press Enter')
-    chat_history = []
+st.subheader('Upload PDF')
+uploaded_file = st.file_uploader('Upload a PDF', type=".pdf")
+show_img = st.empty()
 
-    if st.button('Submit'):
-        if not txt:
-            st.error('Enter text')
-        else:
-            add_text(chat_history, txt)
-            chain = process_file(temp_path)
+submit_btn = st.button('Submit')
 
-            # Set context and prompt template
-            context = " ".join([item[0] for item in chat_history])
-            prompt_template = "The document mentions {}. What would you like to know about it?"
-
-            if chain.retriever.has_data():
-                result = chain({
-                    "question": txt,
-                    'chat_history': chat_history,
-                    'context': context,
-                    'prompt_template': prompt_template
-                }, return_only_outputs=True)
-
-                chat_history.append((txt, result["answer"]))
-
-                # Display chat history
-                for idx, (question, answer) in enumerate(chat_history):
-                    st.text_area(f'{idx + 1}. User:', question, height=50)
-                    st.text_area(f'{idx + 1}. Chatbot:', answer, height=50)
-            else:
-                st.error('The uploaded PDF does not contain any searchable content.')
-
-# PDF Upload Section
-with col2:
-    st.subheader('Upload PDF')
-    uploaded_file = st.file_uploader('Upload a PDF', type=".pdf")
-
-    if uploaded_file:
+if submit_btn:
+    if not uploaded_file:
+        st.error('Upload a PDF file')
+    else:
         with tempfile.NamedTemporaryFile(delete=False) as temp_file:
             temp_path = temp_file.name
             temp_file.write(uploaded_file.read())
 
-        st.success('PDF uploaded successfully!')
+        add_text(chat_history, txt)
+        chain = process_file(temp_path)
 
-st.beta_columns([2, 1])  # Add space between sections
+        # Set context and prompt template
+        context = " ".join([item[0] for item in chat_history])
+        prompt_template = "The document mentions {}. What would you like to know about it?"
+
+        if chain.retriever.has_data():
+            result = chain({
+                "question": txt,
+                'chat_history': chat_history,
+                'context': context,
+                'prompt_template': prompt_template
+            }, return_only_outputs=True)
+
+            chat_history.append((txt, result["answer"]))
+
+            # Display chat history
+            for idx, (question, answer) in enumerate(chat_history):
+                st.text(f"{idx + 1}. User: {question}")
+                st.text(f"   Chatbot: {answer}")
+
+            show_img.image(result["image"], caption="Document Page", use_column_width=True)
+        else:
+            st.error('The uploaded PDF does not contain any searchable content.')
+
+        os.remove(temp_path)
