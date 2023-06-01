e24de80e82d16a25c67b15dbbbdf55cd682fe284
VruttiTanna <tannavrutti@gmail.com>
2023-06-01 19:28:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import streamlit as st
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import PyPDFLoader
 import os
-import fitz
 from PIL import Image
-from io import BytesIO
 
 # Global variables
 COUNT, N = 0, 0
 chat_history = []
-chain = None
+chain = ''
 
 # Function to set the OpenAI API key
 def set_apikey(api_key):
@@ -30,53 +29,45 @@ def process_file(file):
     if 'OPENAI_API_KEY' not in os.environ:
         st.error('Upload your OpenAI API key')
 
-    loader = PyPDFLoader(file.name)
+    loader = PyPDFLoader(file)
     documents = loader.load()
 
     embeddings = OpenAIEmbeddings()
     pdfsearch = Chroma.from_documents(documents, embeddings)
 
-    chain = ConversationalRetrievalChain.from_llm(
-        ChatOpenAI(temperature=0.3),
-        retriever=pdfsearch.as_retriever(search_kwargs={"k": 1}),
-        return_source_documents=True
-    )
+    chain = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.3),
+                                                  retriever=pdfsearch.as_retriever(search_kwargs={"k": 1}),
+                                                  return_source_documents=True)
     return chain
 
 # Function to generate a response based on the chat history and query
-def generate_response(history, query, btn):
+def generate_response(history, query, file):
     global COUNT, N, chat_history, chain
 
-    if not btn:
+    if not file:
         st.error('Upload a PDF')
     if COUNT == 0:
-        chain = process_file(btn)
+        chain = process_file(file)
         COUNT += 1
 
     result = chain({"question": query, 'chat_history': chat_history}, return_only_outputs=True)
     chat_history.append((query, result["answer"]))
     N = list(result['source_documents'][0])[1][1]['page']
 
-    response = ""
     for char in result['answer']:
-        response += char
-        yield (history, response)
+        history[-1] = (history[-1][0], history[-1][1] + char)
+        yield history, ''
 
 # Function to render a specific page of a PDF file as an image
 def render_file(file):
     global N
     try:
-        doc = fitz.open(stream=BytesIO(file.read()), filetype="pdf")
-        page = doc[N]
-        # Render the page as a PNG image with a resolution of 300 DPI
-        pix = page.get_pixmap(matrix=fitz.Matrix(300/72, 300/72))
-        image = Image.frombytes('RGB', [pix.width, pix.height], pix.samples)
+        image = Image.open(file)
         return image
     except FileNotFoundError:
-        st.error('PDF file not found. Please make sure the file exists and check the file path.')
+        raise st.Error('PDF file not found. Please make sure the file exists and check the file path.')
 
 # Streamlit application setup
-st.set_page_config(layout="wide")
 st.title('Chatbot with PDF Support')
 
 # OpenAI API Key
@@ -86,24 +77,19 @@ if api_key:
     set_apikey(api_key)
 
 # Chatbot and Image Display
-col1, col2 = st.beta_columns(2)
-with col1:
-    st.subheader('Chatbot')
-    chat_history_output = st.empty()
-    txt = st.text_input('Enter text and press enter')
-    chat_history = []
+st.subheader('Chatbot')
+chat_history_output = st.empty()
+txt = st.text_input('Enter text and press enter')
+chat_history = []
 
-with col2:
-    st.subheader('Upload PDF')
-    btn = st.file_uploader('Upload a PDF', type=".pdf")
-    show_img = st.empty()
+st.subheader('Upload PDF')
+uploaded_file = st.file_uploader('Upload a PDF', type=".pdf")
+show_img = st.empty()
 
 submit_btn = st.button('Submit')
 
 if submit_btn:
     add_text(chat_history, txt)
-    response_generator = generate_response(chat_history, txt, btn)
-    for history, response in response_generator:
-        chat_history_output.text('\n'.join([f'{h[0]}: {h[1]}' for h in history]))
-        chat_history_output.text(response)
-        show_img.image(render_file(btn), use_column_width=True)
+    for _, _ in generate_response(chat_history, txt, uploaded_file):
+        pass
+    render_file(uploaded_file)
