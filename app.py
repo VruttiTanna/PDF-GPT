4bb63ce7c08c036e695432630eec44fab7e12c39
VruttiTanna <tannavrutti@gmail.com>
2023-06-02 00:55:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,22 +53,23 @@ if api_key:
 
 # Chatbot and Image Display
 st.subheader('Chatbot')
+chat_history_output = st.empty()
 txt = st.text_input('Enter text and press enter')
 chat_history = []
 
 st.subheader('Upload PDF')
-uploaded_file = st.file_uploader('Upload a PDF', type=".pdf")
+btn = st.file_uploader('Upload a PDF', type=".pdf")
 show_img = st.empty()
 
 submit_btn = st.button('Submit')
 
 if submit_btn:
-    if not uploaded_file:
+    if not btn:
         st.error('Upload a PDF file')
     else:
         with tempfile.NamedTemporaryFile(delete=False) as temp_file:
             temp_path = temp_file.name
-            temp_file.write(uploaded_file.read())
+            temp_file.write(btn.read())
 
         add_text(chat_history, txt)
         chain = process_file(temp_path)
@@ -77,7 +78,7 @@ if submit_btn:
         context = " ".join([item[0] for item in chat_history])
         prompt_template = "The document mentions {}. What would you like to know about it?"
 
-        if chain.retriever.has_data():
+        if chain.retriever.vectorstore:
             result = chain({
                 "question": txt,
                 'chat_history': chat_history,
@@ -86,13 +87,7 @@ if submit_btn:
             }, return_only_outputs=True)
 
             chat_history.append((txt, result["answer"]))
-
-            # Display chat history
-            for idx, (question, answer) in enumerate(chat_history):
-                st.text(f"{idx + 1}. User: {question}")
-                st.text(f"   Chatbot: {answer}")
-
-            show_img.image(result["image"], caption="Document Page", use_column_width=True)
+            chat_history_output.write(chat_history[-1][1])
         else:
             st.error('The uploaded PDF does not contain any searchable content.')
 
