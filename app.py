c340a05c8e9496bbeccf4c2f554648a67b738a2f
VruttiTanna <tannavrutti@gmail.com>
2023-06-01 20:32:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,24 @@
 import streamlit as st
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
-import os
-import fitz
-from PIL import Image
 from langchain.document_loaders import PyPDFLoader
-import tempfile
+import os
 
-# Global variables
-COUNT, N = 0, 0
-chat_history = []
-chain = None
 
 # Function to set the OpenAI API key
 def set_apikey(api_key):
     os.environ['OPENAI_API_KEY'] = api_key
 
-# Function to add text to the chat history
-def add_text(history, text):
-    if not text:
-        st.error('Enter text')
-    history.append((text, ''))
-    return history
 
 # Function to process the PDF file and create a conversation chain
-def process_file(file):
+def process_file(file_path):
     if 'OPENAI_API_KEY' not in os.environ:
         st.error('Upload your OpenAI API key')
 
-    # Create a temporary file to store the uploaded PDF
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        temp_path = temp_file.name
-        temp_file.write(file.read())
-
-    # Check if the file is empty
-    if os.path.getsize(temp_path) == 0:
-        os.remove(temp_path)
-        raise st.error('Uploaded PDF file is empty')
-
-    loader = PyPDFLoader(temp_path)
+    loader = PyPDFLoader(file_path)
     documents = loader.load()
 
     embeddings = OpenAIEmbeddings()
@@ -53,47 +31,7 @@ def process_file(file):
         return_source_documents=True
     )
 
-    # Delete the temporary file
-    os.remove(temp_path)
-
     return chain
-
-# Function to render a specific page of a PDF file as an image
-def render_file(file):
-    try:
-        doc = fitz.open(stream=file, filetype="pdf")
-        page = doc[0]  # Render the first page of the PDF
-        # Render the page as a PNG image with a resolution of 300 DPI
-        pix = page.get_pixmap(matrix=fitz.Matrix(300/72, 300/72))
-        image = Image.frombytes('RGB', [pix.width, pix.height], pix.samples)
-        return image
-    except fitz.Error as e:
-        st.error(f"PDF error: {str(e)}")
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
-
-# Rest of the code...
-
-
-
-# Function to generate a response based on the chat history and query
-def generate_response(history, query, btn):
-    global COUNT, N, chat_history, chain
-
-    if not btn:
-        st.error('Upload a PDF')
-    if COUNT == 0:
-        chain = process_file(btn)
-        COUNT += 1
-
-    result = chain({"question": query, 'chat_history': chat_history}, return_only_outputs=True)
-    chat_history.append((query, result["answer"]))
-    N = list(result['source_documents'][0])[1][1]['page']
-
-    for char in result['answer']:
-        history[-1] = (history[-1][0], history[-1][1] + char)
-        yield history, ''
-
 
 
 # Streamlit application setup
@@ -118,9 +56,11 @@ show_img = st.empty()
 submit_btn = st.button('Submit')
 
 if submit_btn:
-    add_text(chat_history, txt)
-    for _, _ in generate_response(chat_history, txt, btn):
-        pass
-    image = render_file(btn)
-    if image:
-        show_img.image(image)
+    if not btn:
+        st.error('Upload a PDF file')
+    else:
+        add_text(chat_history, txt)
+        chain = process_file(btn)
+        result = chain({"question": txt, 'chat_history': chat_history}, return_only_outputs=True)
+        chat_history.append((txt, result["answer"]))
+        chat_history_output.write(chat_history[-1][1])
